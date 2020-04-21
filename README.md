8a2d90f7c526f19a40cb42ebdb195adc67e3fb69
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-04-21 20:43:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gym Manager REST API
+
+REST API for Gym Manager Software - Built with Typecript/Express/Jest/TypeORM...
+
---
+++ test/env.ts
@@ -0,0 +1,7 @@
+process.env.DB_NAME="gym_manager_test"
+process.env.DB_USER="postgres"
+process.env.DB_PASS="root"
+process.env.DB_PORT="5432"
+process.env.DB_DIALECT="postgres"
+
+process.env.JWT_AUTH_SECRET = '8d2be8dybe3n78fn347f3hf834hf389fj3e83he'
